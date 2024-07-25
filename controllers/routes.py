bb13432ef6a383057439893a5cb29da90c74a896
ShamNeko <mkdvlp01@gmail.com>
2024-07-25 21:57:08 +0900

    baseの作成とMODELSでデータ取得

--- controllers/routes.py
+++ controllers/routes.py
@@ -1,8 +1,15 @@
 from flask import Blueprint, render_template
 
+from models import data
+
 main = Blueprint("main", __name__)
 
 
 @main.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", memo_list=data.memo_list)
+
+
+@main.route("/user/<name>")
+def user(name=None):
+    return render_template("user.html", name=name)
---
+++ models/data.py
@@ -0,0 +1,4 @@
+memo_list: list = [
+    {"title": "test01", "body": "mkです。"},
+    {"title": "test01", "body": "mk２です。"}
+]
