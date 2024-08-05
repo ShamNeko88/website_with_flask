7f7b58ba26e344978ba8a87812140168fd7048d4
ShamNeko <mkdvlp01@gmail.com>
2024-08-05 22:00:47 +0900

    ログイン機能実装中（ID、パス　ハードコーディング）

--- controllers/routes.py
+++ controllers/routes.py
@@ -1,51 +1,62 @@
-import os
-
 from flask import Blueprint, render_template, request, redirect
-from flask_login import UserMixin, LoginManager
+from flask_login import login_required, UserMixin, login_user, logout_user
 
 from models import data
 
 main = Blueprint("main", __name__)
-main.secret_key = os.urandom(24)
-login_manager = LoginManager()
-login_manager.init_app(run.app)
+
 
 # メモデータ管理クラスのインスタンス生成
 memo_db = data.MemoManager()
 
 
+# ログインユーザーのインスタンス
 class User(UserMixin):
     def __init__(self, user_id) -> None:
         self.id = user_id
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User(user_id)
+@main.route("/logout", methods=["GET"])
+def logout():
+    logout_user()
+    return redirect("/login")
 
 
 @main.route("/login", methods=["GET", "POST"])
 def login():
     error_message = ""
     user_id = ""
+
+    if request.method == "POST":
+        user_id = request.form.get("user_id")
+        password = request.form.get("password")
+        if (user_id == "admin" and password == "admin"):
+            user = User(user_id)
+            login_user(user)
+            return redirect("/")
+        else:
+            error_message = "入力されたIDもしくはパスワードが正しくありません"
     return render_template(
         "login.html", user_id=user_id, error_message=error_message
     )
 
 
 @main.route("/")
+@login_required
 def index():
     memo_list = memo_db.get_all_memo_data()
     return render_template("index.html", memo_list=memo_list)
 
 
 @main.route("/user/<name>")
+@login_required
 def user(name=None):
     return render_template("user.html", name=name)
 
 
 # メモの登録してメモ一覧に戻る
 @main.route("/regist", methods=["GET", "POST"])
+@login_required
 def regist():
     if request.method == "POST":
         title = request.form.get("title")
@@ -57,6 +68,7 @@ def regist():
 
 # メモの編集してメモ一覧に戻る
 @main.route("/<id>/edit", methods=["GET", "POST"])
+@login_required
 def edit(id):
     if request.method == "POST":
         title = request.form.get("title")
@@ -69,6 +81,7 @@ def edit(id):
 
 # メモを削除してメモ一覧に戻る
 @main.route("/<id>/delete", methods=["GET", "POST"])
+@login_required
 def delete(id):
     if request.method == "POST":
         memo_db.delete_memo_data(id)
--- run.py
+++ run.py
@@ -1,8 +1,25 @@
-from flask import Flask
-from controllers.routes import main
+import os
+
+from flask import Flask, redirect
+from flask_login import LoginManager
+from controllers.routes import main, User
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 app.register_blueprint(main)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
+
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return redirect("/login")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
